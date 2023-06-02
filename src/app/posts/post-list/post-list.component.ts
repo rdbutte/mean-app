acb67c64997802efdada5de9d7a97a213d6a18ce
rdbutte <rdbutte@gmail.com>
2023-06-02 17:01:53 +0530

    basic angular part

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   { title: 'First Post', content: 'This is first post\'s content' },
+  //   { title: 'Second Post', content: 'This is second post\'s content' },
+  //   { title: 'Third Post', content: 'This is third post\'s content' }
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) { }
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { title: title, content: content };
+    this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  }
+
+  getPostUpdatedListener() {
+    return this.postsUpdated.asObservable();
+  }
+  constructor() { }
+}
